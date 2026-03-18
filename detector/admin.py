c067c7e02b20d0294716ac7b3c6eb3a9185cb491
renish <renish7606@oulook.com>
2026-03-18 22:43:19 +0530

    Update database

--- detector/admin.py
+++ detector/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import FoodHistory, UserProfile, Exercise, MealLog
+
+admin.site.register(FoodHistory)
+admin.site.register(UserProfile)
+admin.site.register(Exercise)
+admin.site.register(MealLog)
